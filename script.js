5c7c09fa1ec81e700ef5cb2f7d28e09aa7d47435
M. Maaz <maaz.shakeel.bodla@gmail.com>
2023-09-26 08:49:52 +0700

    feat: write a little psuedocode of it (comments)

---
+++ script.js
@@ -0,0 +1,34 @@
+/**
+ * Define a function to navigate betweens form steps.
+ * It accepts one parameter. That is - step number.
+ */
+const navigateToFormStep = (stepNumber) => {
+  /**
+   * Hide all form steps.
+   */
+  document.querySelectorAll(".form-step").forEach((formStepElement) => {
+    formStepElement.classList.add("d-none");
+  });
+  /**
+   * Mark all form steps as unfinished.
+   */
+  /**
+   * Show the current form step (as passed to the function).
+   */
+  /**
+   * Select the form step circle (progress bar).
+   */
+  /**
+   * Mark the current form step as active.
+   */
+  /**
+   * Loop through each form step circles.
+   * This loop will continue up to the current step number.
+   * Example: If the current step is 3,
+   * then the loop will perform operations for step 1 and 2.
+   */
+};
+
+/**
+ * Select all form navigation buttons, and loop through them.
+ */
